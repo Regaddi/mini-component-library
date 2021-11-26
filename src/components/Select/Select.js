8e4028929cd941675fdfb1a303435da23b478c4a
Christian Stuff <christian@daily.co>
2021-11-26 14:11:47 +0100

    implement Select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,41 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <span aria-hidden>{displayedValue}</span>
+      <Icon aria-hidden id="chevron-down" size={24} strokeWidth={2} />
+      <CustomSelect value={value} onChange={onChange}>
+        {children}
+      </CustomSelect>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  align-items: center;
+  background: ${COLORS.transparentGray15};
+  border-radius: 8px;
+  color: ${COLORS.gray700};
+  display: inline-flex;
+  flex-direction: row;
+  gap: 20px;
+  justify-items: space-between;
+  padding: 12px 16px;
+  position: relative;
+
+  &:focus-within {
+    outline: 2px solid #4374CB;
+  }
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const CustomSelect = styled.select`
+  inset: 0;
+  opacity: 0;
+  position: absolute;
+  width: 100%;
+`
 
 export default Select;
